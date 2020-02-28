a8c6b257ea70a1ab2968dc202953e6745e147208
Deepak Rout <deepakrout@gmail.com>
2020-02-27 21:57:06 -0500

    CRUD in progress

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task }  from './task.model'
+import { CreatetaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(taskService: TasksService){}
+    constructor(private taskService: TasksService){}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.taskService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreatetaskDto): Task {
+        return this.taskService.createTask(createTaskDto);
+    }
+
+    @Delete('/:id')
+    deleteTask(@Param('id') id: string): void {
+        this.taskService.deleteTask(id);
+    }
+
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus }  from './task.model'
+import * as uuid from 'uuid/v1';
+import { CreatetaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+   private tasks: Task[] = [];
+
+   getAllTasks(): Task[] {
+       return this.tasks;
+   }
+
+   getTaskById(id: string): Task {
+      return this.tasks.find(task => task.id === id);
+   }
+
+   createTask(createTaskDto: CreatetaskDto): Task {
+       const {title, description } = createTaskDto;
+
+       const task: Task = {
+           id: uuid(),
+           title,
+           description,
+           status: TaskStatus.OPEN
+       };
+
+       this.tasks.push(task)
+       return task;
+   }
+
+   deleteTask(id: string): void {
+     this.tasks = this.tasks.filter(task => task.id !== id);
+   }
+}
