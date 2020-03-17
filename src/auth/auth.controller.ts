98962bee833780cf9efc1faf486d3440f5842e3b
Deepak Rout <deepakrout@gmail.com>
2020-03-16 21:12:40 -0400

    Auth module created

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) {}
+
+    @Post("/signup")
+    signUp(@Body() authCredentialDto: AuthCredentialDto){
+        return this.authService.signUp(authCredentialDto);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
     constructor(@InjectRepository(UserRepository) 
                 private userRepository: UserRepository) { }
+    
+    async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
+        return await this.userRepository.signUp(authCredentialDto);
+    }            
 }
