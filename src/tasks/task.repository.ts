6e14d1be7bb9c1d3aba9ecd035b3f124c3f3d326
Deepak Rout <deepakrout@gmail.com>
2020-03-28 21:20:28 -0400

    Repository changes

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -3,22 +3,38 @@ import { Task } from './task.entity';
 import { CreatetaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+import { User } from "src/auth/user.entity";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async createTask(createTaskDto: CreatetaskDto): Promise<Task> {
+    async createTask(
+        createTaskDto: CreatetaskDto,
+        user: User
+    ): Promise<Task> {
         const { title, description } = createTaskDto;
         const task = new Task();
         task.title = title;
         task.description = description;
         task.status = TaskStatus.OPEN;
+        task.user = user;
+        console.log(`Saving Task..`, task);
         await task.save();
+
+        delete task.user;
+
         return task;
     }
 
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    async getTasks(
+        filterDto: GetTasksFilterDto,
+        user: User
+    ): Promise<Task[]> {
         const { status, search } = filterDto;
         const query = this.createQueryBuilder('task');
+
+        console.log(`Task query`, query);
+
+        query.where('task.userId = :userId', { userId: user.id });
 
         if (status) {
             query.andWhere('task.status = :status', { status })
