5532d0d2911de87746ee76f8691d63a7ad805135
Deepak Rout <deepakrout@gmail.com>
2020-03-30 23:09:48 -0400

    NestJS upgraded

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,6 +1,8 @@
-import { createParamDecorator } from '@nestjs/common';
-import { User } from './user.entity';
-
-export const GetUser = createParamDecorator((data, req): User => {
-    return req.user;
-})
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { User } from './user.entity'
+ 
+export const GetUser = createParamDecorator(
+  (_, ctx: ExecutionContext): User => {
+    return <User>ctx.switchToHttp().getRequest().user
+  },
+)
